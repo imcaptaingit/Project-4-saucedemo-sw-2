93764c2a933a2b8e91dce8eb663ee572000b4ef8
imcaptaingit <132778127+imcaptaingit@users.noreply.github.com>
2023-06-27 18:48:59 +0100

    LoginTest.java

--- src/testsuite/LoginTest.java
+++ src/testsuite/LoginTest.java
@@ -8,45 +8,42 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class LoginTest extends BaseTest {
-    //set up base url
+    //Declaring BaseURL
     String baseUrl = "https://www.saucedemo.com/";
 
     @Before
-    public void setUpBrowser() {
+    public void setUp() {
         openBrowser(baseUrl);
     }
 
     @Test
-    public void userShouldLoginSuccessfullyWithValidUserShouldLoginSuccessfullyWithValid() {
-        //Enter “standard_user” username
-        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("standard_user");
-        //Enter “secret_sauce” password
-        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("secret_sauce");
-        //Click on ‘LOGIN’ button
-        driver.findElement(By.xpath("//input[@id='login-button']")).click();
-        //Verify the text “Products”
-        WebElement productText = driver.findElement(By.xpath("//span[@class='title']"));
-        String text = productText.getText();
-        System.out.println(text);
-        Assert.assertEquals("Products", text);
+    public void userShouldLoginSuccessfullyWithValidCredentials() {
+        //enter username and password
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        String expected = "Products";
+        String actual = driver.findElement(By.xpath("//span[text() = 'Products']")).getText();
     }
 
     @Test
     public void verifyThatSixProductsAreDisplayedOnPage() {
-        //Enter “standard_user” username
-        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("standard_user");
-        //Enter “secret_sauce” password
-        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("secret_sauce");
-        //Click on ‘LOGIN’ button
-        driver.findElement(By.xpath("//input[@id='login-button']")).click();
-        //Verify that six products are displayed on the page
-        WebElement sixProductsDisplayed = driver.findElement(By.xpath("//div[@class='inventory_list']"));
-        String text = sixProductsDisplayed.getText();
-        System.out.println(text);
+        //enter username and password
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        List<WebElement> products = driver.findElements(By.xpath("//div[@class = 'inventory_item']"));
+        int expected = 6;
+        int actual = products.size();
+        Assert.assertEquals(expected,actual);
     }
+
     @After
-    public void close() {
+    public void tearDown() {
         closeBrowser();
     }
+
 }
